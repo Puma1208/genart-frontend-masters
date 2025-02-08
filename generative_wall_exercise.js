a229f31f4698c1e9c901362cd5eb98ecd68d3fc1
Puma1208 <margarita.naryzhnyaya@gmail.com>
2025-02-08 15:52:58 +0100

    Added way to choose 2 random points not overlapping

--- generative_wall_exercise.js
+++ generative_wall_exercise.js
@@ -8,11 +8,50 @@ const settings = {
 };
 
 const sketch = () => {
-    const createGrid=() => {
+    const makeOrderedTrapezoids = (grid_count) => {
+        // Steps
+        //  1. (Remove points that are on the last y line - because otherwise not a trapezoid but a triangle or a line)
+        //  2.  Iterate over the points until no more points available
+        //      Potential improvements but not explicit:
+        //          - No 2 points on the same vertical line - otherwise not a trapezoid
+        //          - No 2 points the same - not a trapezoid
+        const trapezoids = []
+        const points = createGrid(grid_count);
+        const valid_points = points.slice();
+        let a = 0;
+        var averages = [];
+        while (valid_points.length>=1){
+            const point_1 = random.pick(valid_points);
+            valid_points.pop(valid_points.indexOf(point_1));
+            const point_2 = random.pick(valid_points);
+            while (point_2.position[0]==point_1[0]){
+                point_2 = random.pick(valid_points);
+            }
+            valid_points.pop(valid_points.indexOf(point_2));
+            const average_y = (point_1.position[1]+point_2.position[1])/2;
+            averages.push(average_y);
+            trapezoids.push({
+                p1:point_1,
+                p2:point_2,
+                p3:[point_2.position[0], 1],
+                p4:[point_1.position[0], 1],
+                average_y:average_y
+            })
+            
+        }
+        // Sort array of trapezoids by array of averages
+        // https://stackoverflow.com/a/44063445
+        console.log(trapezoids.map(t => t.average_y));
+        
+        trapezoids.sort((a, b) => a.average_y-b.average_y);
+        console.log(trapezoids)
+        
+    }
+    const createGrid=(count) => {
         console.log("Inside the function");
         const points = [];
         const chosen_points = [];
-        const count = 6;
+        // const count = 6;
 
 
         for (let x=0; x<count;x++){
@@ -29,19 +68,22 @@ const sketch = () => {
         return points
     };
     // random.setSeed(42);
-    const points = createGrid();
+    const points = createGrid(6);
+    
     const margin =300;
+
+    makeOrderedTrapezoids(6);
     
     return ({ context, width, height }) => {
         context.fillStyle = 'white';
         context.fillRect(0, 0, width, height);
         
         // Choose n trapezoids to draw - or until the all grid points are exhausted
-        const count = 1;
-        //      Draw a line between the two
-        //      Draw the parallel sides extending down
-        //      Fill the trapezoid
-        //      Stroke with the background color
+        const count = 10;
+        //      ✅ Draw a line between the two 
+        //      ✅ Draw the parallel sides extending down
+        //      ✅ Fill the trapezoid
+        //      ✅ Stroke with the background color
     
         //      Later: layer the shapes by the average Y position of their two grid points
         
@@ -51,8 +93,8 @@ const sketch = () => {
             const point1 = random.pick(points);
             const point2 = random.pick(points);
             const radius = point1.radius;
-            console.log("points: ("+point1.position[0] +", " + point1.position[1] + ") tadaaa");
-            console.log("points: ("+point2.position[0] +", " + point2.position[1] + ") tadaaa");
+            // console.log("points: ("+point1.position[0] +", " + point1.position[1] + ") tadaaa");
+            // console.log("points: ("+point2.position[0] +", " + point2.position[1] + ") tadaaa");
         
             const x1 = lerp(margin, width-margin, point1.position[0]);
             const y1 = lerp(margin, height-margin, point1.position[1]);
@@ -84,8 +126,11 @@ const sketch = () => {
             // context.lineTo(lerp(margin, width-margin, point4[0]), lerp(margin, height-margin, point4[1]));
             // context.lineTo(lerp(margin, width-margin, point3[0]), lerp(margin, height-margin, point3[1]));
             
-            context.strokeStyle = 'pink';
+            context.strokeStyle = 'white';
             context.lineWidth = 5;
+            context.fillStyle = 'grey';
+            context.fill();
+            
             context.stroke();
             context.closePath();
 
